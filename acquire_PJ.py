73ac9aee4c81cc5a6d030ba328e1fe5674f986bf
Paige Guajardo <paigeeguajardo@gmail.com>
2022-01-04 16:27:54 -0600

    Uploaded acquire and explore files

---
+++ acquire_PJ.py
@@ -0,0 +1,60 @@
+import env
+import pandas as pd
+import os
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+#Scaling Processers
+import sklearn.preprocessing
+
+from datetime import date
+
+#Gets connection to Code Up database using env file
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+
+# Get zillow.csv Data
+def get_zillow_data():
+    filename = "zillow.csv"
+
+    if os.path.isfile(filename):
+        zillow = pd.read_csv(filename)
+    else:
+        zillow = pd.read_sql("""
+SELECT prop.*, 
+       pred.logerror, 
+       pred.transactiondate, 
+       air.airconditioningdesc, 
+       arch.architecturalstyledesc, 
+       build.buildingclassdesc, 
+       heat.heatingorsystemdesc, 
+       landuse.propertylandusedesc, 
+       story.storydesc, 
+       construct.typeconstructiondesc 
+FROM   properties_2017 prop  
+       INNER JOIN (SELECT parcelid,
+       					  logerror,
+                          Max(transactiondate) transactiondate 
+                   FROM   predictions_2017 
+                   GROUP  BY parcelid, logerror) pred
+               USING (parcelid) 
+       LEFT JOIN airconditioningtype air USING (airconditioningtypeid) 
+       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid) 
+       LEFT JOIN buildingclasstype build USING (buildingclasstypeid) 
+       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid) 
+       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid) 
+       LEFT JOIN storytype story USING (storytypeid) 
+       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid) 
+WHERE  prop.latitude IS NOT NULL 
+       AND prop.longitude IS NOT NULL AND transactiondate <= '2017-12-31';
+""", 
+        get_connection('zillow'))
+        zillow.to_csv(index = False)
+    return zillow
